11af21bdcf9a4d3c0841ab4ce571879f1a4fa37e
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-04 22:03:47 +0100

    added styles to input

--- src/modules/HomeModule/components/Slider/Slider.tsx
+++ src/modules/HomeModule/components/Slider/Slider.tsx
@@ -1,30 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Slider.scss";
 
 export const Slider: React.FC = () => {
-	const [passwordLength, setPasswordLength] = useState(6);
+	const [passwordLength, setPasswordLength] = useState<number>(6);
 
 	const handleSliderChange = (event: React.FormEvent<HTMLInputElement>) => {
 		const newValue = event.currentTarget.valueAsNumber;
 		setPasswordLength(newValue);
+
+		const input = event.currentTarget;
+		input.style.setProperty("--value", input.value);
 	};
+
+	useEffect(() => {
+		const input = document.querySelector<HTMLInputElement>(
+			".slider__input"
+		);
+		if (!input) return;
+
+		input.style.setProperty(
+			"--value",
+			input.getAttribute("value") || passwordLength.toString()
+		);
+		input.style.setProperty("--min", input.getAttribute("min") || "4");
+		input.style.setProperty("--max", input.getAttribute("max") || "16");
+
+		input.addEventListener("input", () => {
+			input.style.setProperty("--value", input.value);
+		});
+
+		return () => {
+			input.removeEventListener("input", () => {
+				input.style.setProperty("--value", input.value);
+			});
+		};
+	}, [passwordLength]);
 
 	return (
 		<label className={"slider"}>
-			<span
-				className={"slider__title"}
-			>
-				<span
-					className={"slider__text"}
-				>
-				Character Length
-				</span>
-				<span
-					className={"slider__value"}
-				>
-				{passwordLength}
-				</span>
-			</span>
+      <span className={"slider__title"}>
+        <span className={"slider__text"}>Character Length</span>
+        <span className={"slider__value"}>{passwordLength}</span>
+      </span>
 			<input
 				className={"slider__input"}
 				type="range"
