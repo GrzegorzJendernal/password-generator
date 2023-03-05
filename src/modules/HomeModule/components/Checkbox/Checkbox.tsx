d2e71bfbe04be828077224f41d34e3cbe0d72763
GrzegorzJendernal <g.jendernal@gmail.com>
2023-03-05 21:10:38 +0100

    styled and added functionality to checkbox

--- src/modules/HomeModule/components/Checkbox/Checkbox.tsx
+++ src/modules/HomeModule/components/Checkbox/Checkbox.tsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Checkbox.scss";
 
 interface CheckboxProps {
 	text: string;
 }
 export const Checkbox: React.FC<CheckboxProps> = ({text}) => {
+	const [isChecked, setIsChecked] = useState(false);
 
 	return (
-		<label className={"checkbox"}>
-		<input className={"checkbox__input"} type={"checkbox"}/>
+		<div className={"checkbox"}>
+		<button
+			className={`checkbox__button ${isChecked ? "checkbox__button--checked" : ""}`}
+			onClick={() => setIsChecked(!isChecked)}
+		>
+			{isChecked ? <img className={"checkbox__mark"} src={"/src/assets/images/checked.svg"} alt={"checked"}/> : ""}
+		</button>
 			<span className={"checkbox__text"}>
 				Include {text}
 			</span>
-		</label>
+		</div>
 	);
 };
